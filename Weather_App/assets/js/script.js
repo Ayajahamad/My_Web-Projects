42d9bb6a50eea13bef664c78f826d6342bc476ca
Ayajahamad <ayazahmad2362001@gmail.com>
2024-02-11 23:39:02 +0530

    Weather app is added Using API

---
+++ Weather_App/assets/js/script.js
@@ -0,0 +1,63 @@
+let api = "https://api.openweathermap.org/data/2.5/weather?units=metric&"
+let code = "3951ba7b625400cef40215a9cd21cc93"
+
+let city = document.querySelector(".search input")
+let img = document.querySelector(".weather-img")
+let icon = document.querySelector('.icon')
+let h4 = document.querySelector('.h4')
+
+async function weatherFind(){
+    const response = await fetch(`${api}` + `q=${city.value}` + `&appid=${code}`)
+    const data = await response.json()
+
+    console.log(response)
+    console.log(data)
+
+    let temp = document.querySelector(".temprechure")
+    let cuntry = document.querySelector(".cuntry")
+    let windspeed = document.querySelector(".windspeed")
+    let humidity = document.querySelector(".humidity")
+
+    temp.innerHTML = `${data.main.temp}°C`
+    cuntry.innerHTML = data.name
+    windspeed.innerHTML = `${data.wind.speed}km/h`
+    humidity.innerHTML = `${data.main.humidity}%`
+    console.log(data.cod)
+
+   if(data.cod == 200){
+    switch (true) {
+        case data.weather[0].main == "Clouds":
+            img.src = "assets/images/clouds.png"
+            break;
+        case data.weather[0].main == "Clear":
+            img.src = "assets/images/clear.png"
+            break;
+        case data.weather[0].main == "Rain":
+            img.src = "assets/images/rain.png"
+            break;
+        case data.weather[0].main == "Drizzle":
+            img.src = "assets/images/drizzle.png"
+            break;
+        case data.weather[0].main == "Mist":
+            img.src = "assets/images/mist.png"
+            break;
+        case data.weather[0].main == "Snow":
+            img.src = "assets/images/snow.png"
+            break;
+            case data.wcod !== 200:
+            h4.style.display = "block"
+            console.log(data)
+            break;
+        default:     
+            break;
+    }
+    }
+    else{
+        h4.style.display = "block"
+    console.log(data)
+    }
+   }
+
+icon.addEventListener('click',()=>{
+    weatherFind()
+})

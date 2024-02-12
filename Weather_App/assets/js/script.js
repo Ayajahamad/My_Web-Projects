5c732ab6cee98ae3d71e1e6168d055a921c06185
Ayajahamad <ayazahmad2362001@gmail.com>
2024-02-12 10:59:28 +0530

    If Error in Address It Show the error

--- Weather_App/assets/js/script.js
+++ Weather_App/assets/js/script.js
@@ -10,51 +10,51 @@ async function weatherFind(){
     const response = await fetch(`${api}` + `q=${city.value}` + `&appid=${code}`)
     const data = await response.json()
 
-    console.log(response)
-    console.log(data)
-
     let temp = document.querySelector(".temprechure")
     let cuntry = document.querySelector(".cuntry")
     let windspeed = document.querySelector(".windspeed")
     let humidity = document.querySelector(".humidity")
 
-    temp.innerHTML = `${data.main.temp}°C`
-    cuntry.innerHTML = data.name
-    windspeed.innerHTML = `${data.wind.speed}km/h`
-    humidity.innerHTML = `${data.main.humidity}%`
-    console.log(data.cod)
-
-   if(data.cod == 200){
-    switch (true) {
-        case data.weather[0].main == "Clouds":
-            img.src = "assets/images/clouds.png"
-            break;
-        case data.weather[0].main == "Clear":
-            img.src = "assets/images/clear.png"
-            break;
-        case data.weather[0].main == "Rain":
-            img.src = "assets/images/rain.png"
-            break;
-        case data.weather[0].main == "Drizzle":
-            img.src = "assets/images/drizzle.png"
-            break;
-        case data.weather[0].main == "Mist":
-            img.src = "assets/images/mist.png"
-            break;
-        case data.weather[0].main == "Snow":
-            img.src = "assets/images/snow.png"
-            break;
-            case data.wcod !== 200:
-            h4.style.display = "block"
-            console.log(data)
-            break;
-        default:     
-            break;
-    }
+    if(response.status == 404 || response.status == 400){
+        h4.style.display = "block"
+        temp.innerHTML = ""
+        cuntry.innerHTML = ""
+        windspeed.innerHTML = ""
+        humidity.innerHTML = ""
+        img.src = "/"
     }
     else{
-        h4.style.display = "block"
-    console.log(data)
+        h4.style.display = "none"
+
+        temp.innerHTML = `${data.main.temp}°C`
+        cuntry.innerHTML = data.name
+        windspeed.innerHTML = `${data.wind.speed}km/h`
+        humidity.innerHTML = `${data.main.humidity}%`
+
+   
+        switch (true) {
+            case data.weather[0].main == "Clouds":
+                img.src = "assets/images/clouds.png"
+                break;
+            case data.weather[0].main == "Clear":
+                img.src = "assets/images/clear.png"
+                break;
+            case data.weather[0].main == "Rain":
+                img.src = "assets/images/rain.png"
+                break;
+            case data.weather[0].main == "Drizzle":
+                img.src = "assets/images/drizzle.png"
+                break;
+            case data.weather[0].main == "Mist":
+                img.src = "assets/images/mist.png"
+                break;
+            case data.weather[0].main == "Snow":
+                img.src = "assets/images/snow.png"
+                break;
+            default:    
+                img.src = "assets/images/drizzle.png" 
+                break;
+        }
     }
    }
 
